114ce05d27f56f80721723d3a83e51358dc44b08
angst <angst>
2023-09-10 13:06:19 +0200

    format

--- svelte.config.js
+++ svelte.config.js
@@ -11,13 +11,13 @@ const config = {
 		alias: {
 			$components: 'src/lib/components',
 			$utils: 'src/lib/utils.ts',
-			$validation: 'src/validation'
+			$validation: 'src/validation',
 		},
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter(),
+	},
 };
 
 export default config;
