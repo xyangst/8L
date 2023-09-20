702324b1a1750ec3fde6314950721b50bb484cca
angst <angst>
2023-09-20 23:49:06 +0200

    change max toast amount because its stupid

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <slot />
-<Toast />
+<Toast max={99} />
 <footer class="fixed bottom-0 left-1/2 transform -translate-x-1/2 text-center rounded-t-xl p-1">
 	made with :3 by angst
 </footer>
