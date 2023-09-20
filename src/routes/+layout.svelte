ba58329dfc5982ff8c3822ea58416f8d4a68c9fe
angst <angst>
2023-09-20 17:19:36 +0200

    delete everything

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script>
 	import Toaster from '$components/melt/Toaster.svelte';
-	import '../app.css';
+	import '../app.postcss';
 </script>
 
 <slot />
 <Toaster />
-<footer class="fixed bottom-0 left-1/2 transform -translate-x-1/2 text-center bg-background rounded-t-xl p-1">
+<footer
+	class="fixed bottom-0 left-1/2 transform -translate-x-1/2 text-center bg-background rounded-t-xl p-1"
+>
 	made with :3 by angst
 </footer>
