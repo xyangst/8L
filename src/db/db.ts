070351cf31ed03185df71dba81185923d56cbdb0
angst <angst>
2023-09-22 17:29:25 +0200

    remove comment out

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,9 +1,9 @@
 import { createClient } from '@libsql/client';
 import { drizzle } from 'drizzle-orm/libsql';
 import * as schema from './schema';
-export const client = createClient({ url: 'http://127.0.0.1:8080' });
-	// process.env.MODE == 'PROD'
-	// 	? createClient({ url: process.env.LIBSQL_URL!, authToken: process.env.LIBSQL_AUTH! })
-	// 	: createClient({ url: 'http://127.0.0.1:8080' });
+export const client =
+	process.env.MODE == 'PROD'
+		? createClient({ url: process.env.LIBSQL_URL!, authToken: process.env.LIBSQL_AUTH! })
+		: createClient({ url: 'http://127.0.0.1:8080' });
 
 export const db = drizzle(client, { schema });
