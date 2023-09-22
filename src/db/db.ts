60f96996750d5ecca89fedc229a971049bc156c5
angst <angst>
2023-09-22 17:41:17 +0200

    vite-node at migration to simplify env variables

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,9 +1,11 @@
+import { dev } from '$app/environment';
+import { LIBSQL_AUTH, LIBSQL_URL } from '$env/static/private';
 import { createClient } from '@libsql/client';
 import { drizzle } from 'drizzle-orm/libsql';
 import * as schema from './schema';
-export const client =
-	process.env.MODE == 'PROD'
-		? createClient({ url: process.env.LIBSQL_URL!, authToken: process.env.LIBSQL_AUTH! })
-		: createClient({ url: 'http://127.0.0.1:8080' });
+
+export const client = !dev
+	? createClient({ url: LIBSQL_URL, authToken: LIBSQL_AUTH })
+	: createClient({ url: 'http://127.0.0.1:8080' });
 
 export const db = drizzle(client, { schema });
